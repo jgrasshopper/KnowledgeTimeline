685bba890accc0b33f4153f2c71dc82b5b518e56
jgrasshopper <66781543+jgrasshopper@users.noreply.github.com>
2021-01-14 13:45:09 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// We are garbbing all the timeline with list items and we are putting it in a variables called "items"
+const items = document.querySelectorAll('#timeline li');
+
+const isInViewport = el => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+// We have a function called "run" which is going through each item and it's checking to see if it's in the viewport and if it is in the viewport, it will add the class of "show"
+const run = () =>
+  items.forEach(item => {
+    if (isInViewport(item)) {
+      item.classList.add('show');
+    }
+  });
+
+// Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
